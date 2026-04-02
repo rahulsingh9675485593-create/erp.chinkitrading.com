024db94a3244cc79f0e126bba6e125e14ea0022e
Rahul Singh <rahulsingh9675485593@gmail.com>
2026-04-02 19:22:07 +0530

    Pushed complete ERP application code to src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// Complete ERP application code 
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>ERP Application</h1>
+    </div>
+  );
+};
+
+export default App;
